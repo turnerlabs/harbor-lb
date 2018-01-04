1fe3804b90d43a9319123e0b434f33f150a619da
John Ritsema <john.ritsema@turner.com>
2018-01-04 11:53:34 -0500

    hooked up lb.harbor.turnerlabs.io

--- main.tf
+++ main.tf
@@ -4,10 +4,14 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    region  = "us-east-1"
-    bucket  = "tf-state-harbor-lb"
-    key     = "terraform.tfstate"
+    region = "us-east-1"
+    bucket = "tf-state-harbor-lb"
+    key    = "terraform.tfstate"
   }
+}
+
+variable "app" {
+  default = "lb.harbor.turnerlabs.io"
 }
 
 variable "tags" {
@@ -15,7 +19,7 @@ variable "tags" {
 
   default = {
     team          = "cloudarch"
-    application   = "harbor-lb"
+    application   = "lb.harbor.turnerlabs.io"
     environment   = "prod"
     contact-email = "john.ritsema@turner.com"
     customer      = "cloudarch"
@@ -24,8 +28,8 @@ variable "tags" {
 
 # s3 bucket with http serving enabled
 resource "aws_s3_bucket" "main" {
-  bucket = "harbor-lb"
-  tags = "${var.tags}"
+  bucket = "${var.app}"
+  tags   = "${var.tags}"
 
   website {
     index_document = "index.html"
@@ -81,4 +85,14 @@ data "aws_iam_policy_document" "s3" {
 }
 
 # route53 cname to s3 (lb.harbor.turnerlabs.io)
+data "aws_route53_zone" "app" {
+  name = "turnerlabs.io."
+}
 
+resource "aws_route53_record" "record" {
+  zone_id = "${data.aws_route53_zone.app.zone_id}"
+  type    = "CNAME"
+  name    = "${var.app}"
+  records = ["${aws_s3_bucket.main.website_endpoint}"]
+  ttl     = "30"
+}
